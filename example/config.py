2034a4b7daee3f6750951182d1daad8303fb709c
Markus Törnqvist <mjt@fadconsulting.com>
2012-04-26 00:08:55 +0300

    Just use regular UPPERCASE_VARIABLE defs in config

--- example/config.py
+++ example/config.py
@@ -1,12 +1,10 @@
 # vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8 filetype=python
 
-__doc__ = """
-Because we get called by execfile() and its globals, we can access os and environ
-"""
+import os
 
 home = os.path.expanduser('~')
 
-environ['DHP_ROOT'] = os.path.join(home, 'src', 'git_checkouts', 'dhp', 'example')
+DHP_ROOT = os.path.join(home, 'src', 'git_checkouts', 'dhp', 'example')
 
 # EOF
 
