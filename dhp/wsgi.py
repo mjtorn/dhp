da2bcd4ffc9b59d3f9a0c722552aada98c8b23a4
Markus Törnqvist <mjt@fadconsulting.com>
2012-04-25 22:48:57 +0300

    Execute config

--- dhp/wsgi.py
+++ dhp/wsgi.py
@@ -4,13 +4,19 @@ import os
 
 class WSGIHandler():
     def __call__(self, environ, start_response):
+        dhp_config = environ['DHP_CONFIG']
+        execfile(dhp_config)
+
         dhp_root = environ['DHP_ROOT']
 
         file_to_serve = environ.get('PATH_INFO', '/index.dhp')
         file_to_serve = file_to_serve[1:]
         path_to_serve = os.path.join(dhp_root, file_to_serve)
 
-        if os.path.exists(path_to_serve):
+        if os.path.exists(path_to_serve) and path_to_serve == dhp_config:
+            status = '403 FORBIDDEN'
+            output = 'Do not access config'
+        elif os.path.exists(path_to_serve):
             status = '200 OK'
             # XXX: chunks?
             output = open(path_to_serve, 'rb').read()
