d4fd69166d5be6104947d022fbe32212f179823a
Markus Törnqvist <mjt@fadconsulting.com>
2012-04-28 16:24:44 +0300

    get_file_to_serve helper, 99% surely gets deprecated

--- dhp/utils.py
+++ dhp/utils.py
@@ -2,14 +2,23 @@
 
 import os
 
-def get_path_to_serve(request):
-    """To keep get_response simpler
+def get_file_to_serve(request):
+    """Get just the file
     """
 
     environ = request.environ
 
     file_to_serve = environ.get('PATH_INFO', '/index.dhp')
     file_to_serve = file_to_serve[1:]
+
+    return file_to_serve
+
+def get_path_to_serve(request):
+    """To keep get_response simpler
+    """
+
+    file_to_serve = get_file_to_serve(request)
+
     path_to_serve = os.path.join(request.dhp_root, file_to_serve)
 
     return path_to_serve
