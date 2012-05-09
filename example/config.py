2dbfca62c218b7f1b630a91aba3dec095a15106c
Markus Törnqvist <mjt@fadconsulting.com>
2012-05-09 22:29:28 +0300

    Install more middleware

--- example/config.py
+++ example/config.py
@@ -101,17 +101,15 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
-#### UNSUPPORTED
-####
-##MIDDLEWARE_CLASSES = (
-##    'django.middleware.common.CommonMiddleware',
-##    'django.contrib.sessions.middleware.SessionMiddleware',
-##    'django.middleware.csrf.CsrfViewMiddleware',
-##    'django.contrib.auth.middleware.AuthenticationMiddleware',
-##    'django.contrib.messages.middleware.MessageMiddleware',
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
 ##    # Uncomment the next line for simple clickjacking protection:
 ##    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-##)
+)
 
 #### UNSUPPORTED
 ####
